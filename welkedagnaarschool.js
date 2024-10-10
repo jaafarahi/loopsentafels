c90ee4e6311dcba38bf53fa68b574af23398214d
jaafarahi <37632@ma-web.nl>
2024-10-10 11:30:28 +0200

    Create welkedagnaarschool.js

---
+++ welkedagnaarschool.js
@@ -0,0 +1,8 @@
+const huidigeDag = "maandag";
+
+if (huidigeDag == "maandag" || huidigeDag == "dinsdag" || huidigeDag == "woensdag" || huidigeDag == "donderdag" || huidigeDag == "vrijdag") {
+    console.log("Ik moet naar school");
+} else if (huidigeDag == "zaterdag" || huidigeDag == "zondag") {
+    console.log("Lekker uitslapen!");
+}
+
